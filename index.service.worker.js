bdbd84c684de58b48397ff57aa066851867aee13
Osess <osess.li@gmail.com>
2025-08-08 15:27:53 +1200

    test002

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1754293922|90268574';
+const CACHE_VERSION = '1754311522|1763620593';
 /** @type {string} */
 const CACHE_PREFIX = 'GreenLippedMusse-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
